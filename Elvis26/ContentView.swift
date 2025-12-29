729a7d3b6dbb7d9ef561b1c8798666d1b1744619
Akhil Eaga <akhil.eaga@bhp.com>
2025-12-29 11:00:05 +0800

    Initial Commit

---
+++ Elvis26/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Elvis26
+//
+//  Created by Akhil Eaga on 29/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Elvis26/Elvis26App.swift
@@ -0,0 +1,17 @@
+//
+//  Elvis26App.swift
+//  Elvis26
+//
+//  Created by Akhil Eaga on 29/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct Elvis26App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
